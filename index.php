3722f2877b0b1413fd346cea4a14e3d9b2ef871d
Xander Apponi <xapponi@gmail.com>
2018-06-01 14:34:34 -0500

    Separated index.php into components and added basic content

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
-<html>
+<?php get_header(); ?>
 
-  <head>
-    <title>Bit To Byte</title>
-  </head>
+	<div class="row">
 
-  <body>
-    This is the content
-  </body>
-  
-</html>
+		<div class="col-sm-8 blog-main">
+
+			<?php
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+				get_template_part( 'content', get_post_format() );
+
+			endwhile; endif;
+			?>
+
+		</div> <!-- /.blog-main -->
+
+		<?php get_sidebar(); ?>
+
+	</div> <!-- /.row -->
+
+<?php get_footer(); ?>
